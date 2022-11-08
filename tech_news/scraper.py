0ccd0ca9ea96512213ddfea56ef04dcb146c3dc8
Davi <df828316@gmail.com>
2022-11-08 18:19:36 -0300

    feat: adds function scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,11 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_url = (
+      selector.css("div.nav-links a.next.page-numbers::attr(href)").get()
+    )
+    return next_page_url
 
 
 # Requisito 4
