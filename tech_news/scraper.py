90c79ed17f1afe168fed9442302a330cd80abc81
Davi <df828316@gmail.com>
2022-11-09 17:39:41 -0300

    feat: add function scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import time
 from parsel import Selector
@@ -7,7 +8,8 @@ from parsel import Selector
 def fetch(url):
     try:
         response = requests.get(
-          url, headers={"user-agent": "Fake user-agent"}, timeout=3)
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
         if response.status_code != 200:
             return None
         html_content = response.text
@@ -19,23 +21,53 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    return (
-      Selector(html_content).css(".entry-title a::attr(href)").getall()
-    )
+    return Selector(html_content).css(".entry-title a::attr(href)").getall()
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(html_content)
-    next_page_url = (
-      selector.css("div.nav-links a.next.page-numbers::attr(href)").get()
-    )
+    next_page_url = selector.css(
+        "div.nav-links a.next.page-numbers::attr(href)"
+    ).get()
     return next_page_url
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    page_info = {}
+    page_info["url"] = selector.css(
+        "head link[rel=canonical]::attr(href)"
+    ).get()
+    page_info["title"] = selector.css("h1.entry-title::text").get().strip()
+    page_info["timestamp"] = selector.css(
+        "ul.post-meta li.meta-date::text"
+    ).get()
+    page_info["writer"] = selector.css(
+        "ul.post-meta span.author a.url.fn.n::text"
+    ).get()
+    page_info["comments_count"] = selector.css(
+        "div#comments h5.title-block::text"
+    ).get()
+    if page_info["comments_count"] is None:
+        page_info["comments_count"] = 0
+    else:
+        # https://pythonexamples.org/python-regex-extract-find-all-the-numbers-in-string/#:~:text=Python%20Regex%20%E2%80%93%20Get%20List%20of,single%20digit%20in%20the%20string.
+        str = page_info["comments_count"]
+        extract_number_regex = re.findall("[0-9]+", str)
+        page_info["comments_count"] = extract_number_regex[0]
+    page_info["summary"] = "".join(
+        selector.css("div.entry-content > p:nth-of-type(1) *::text").getall()
+    ).strip()
+    print(page_info["summary"])
+    page_info["tags"] = selector.css(
+        "section.post-tags ul li a[rel=tag]::text"
+    ).getall()
+    page_info["category"] = selector.css(
+        "div.entry-details div.meta-category a.category-style span.label::text"
+    ).get()
+    return page_info
 
 
 # Requisito 5
