2669dd2ae3e975e8200b1d0ff4f837acb6e5fcdb
Davi <df828316@gmail.com>
2022-11-11 16:36:59 -0300

    feat: function top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+from operator import itemgetter
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    database_result = search_news({})
+    database_result.sort(key=lambda x: x.get('title'))
+    database_result.sort(key=lambda x: x.get('comments_count'), reverse=True)
+    result = []
+    for news in database_result:
+        if news:
+            result.append((news["title"], news["url"]))
+    return result[:5]
 
 
 # Requisito 11
